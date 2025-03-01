92952bab75549e25ad22ae8acf422e7a6bbe1b6c
AngelOfSol <julietckilian@gmail.com>
2025-02-28 20:52:55 -0500

    Stubbed out grd, and adjusted some styling things for responsiveness.

--- app/src/components/ComboCard.tsx
+++ app/src/components/ComboCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, Center, Clipboard, Flex, Link, Tag, Text, Tooltip } from "@chakra-ui/react";
+import { Badge, Box, Button, Card, Center, Clipboard, Flex, Link, Stat, Tag, Text, Tooltip } from "@chakra-ui/react";
 import { Combo } from "../App";
 import { LuCheck, LuChevronRight, LuClipboard } from "react-icons/lu";
 
@@ -10,15 +10,28 @@ function ComboCard({ combo }: Props) {
 
   return <Card.Root width="100%">
     <Card.Body>
-      <Flex flexDirection="row" alignItems="center">
+      <Flex flexDirection={{ base: "row", mdDown: "column" }} gap={5} alignItems="center">
         <Box flex="1">
-          <Tag.Root ><Tag.Label>{combo.position}</Tag.Label></Tag.Root>
+          <Tag.Root size="lg"><Tag.Label>{combo.position}</Tag.Label></Tag.Root>
         </Box>
         <Box flex="2" >
           <Center>
-            <Flex flexDirection="column">
-              <Text>Damage: {combo.damage}</Text>
-              <Text>Meter: {combo.meter}</Text>
+            <Flex flexDirection="row" gap={4}>
+              <Stat.Root>
+                <Stat.Label>Damage</Stat.Label>
+                <Stat.ValueText>{combo.damage}</Stat.ValueText>
+              </Stat.Root>
+              <Stat.Root>
+                <Stat.Label>Meter</Stat.Label>
+                <Stat.ValueText>{(combo.meter / 100).toFixed(2)}</Stat.ValueText>
+              </Stat.Root>
+              <Stat.Root >
+                <Stat.Label>Grd</Stat.Label>
+                <Stat.ValueText alignItems="baseline">
+                  +2.75<Stat.ValueUnit>blocks</Stat.ValueUnit>
+                </Stat.ValueText>
+              </Stat.Root>
+
             </Flex>
           </Center>
         </Box>
